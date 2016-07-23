b5d0c5e05f28eaee161f97db84bcba5cd3e56c80
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-22 22:22:47 -0400

    Solve Problems 00-02

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(f)
+  return ((f-32)/1.8).round(1)
+end
+
+def ctof(c)
+  return ((c*1.8)+32).round(1)
+end
---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(a, b)
+  return a + b
+end
+
+def subtract(a,b)
+  return a-b
+end
+
+def sum(list)
+  sum = 0
+  list.each do |i|
+    sum += i
+  end
+  return sum
+end
