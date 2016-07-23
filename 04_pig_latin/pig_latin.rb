c6e32d5f8ab032c56d9915a33ae3b922da5f5d85
Kevin Rocker <kevin.j.rocker@gmail.com>
2016-07-22 23:19:47 -0400

    Solve problem 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,21 +1,22 @@
 def translate_one_word(w)
   vowels = ["a", "e", "i", "o", "u"]
-  first_vowel_to_end = w[/[aeiou](.){1,}/]
+  first_vowel_to_end = w[/[aeiou](.)*/]
   if first_vowel_to_end == w
     return "#{w}ay"
-  elsif w[0..1] == "qu"
-    first_chars = "qu"
-    first_vowel_to_end = first_vowel_to_end[1..-1]
-    return "#{first_vowel_to_end}#{first_chars}ay"
   else
     i = w.index(first_vowel_to_end)
     first_chars = w[0..(i-1)]
+    if first_chars[-1] == "q" && first_vowel_to_end[0] == "u"
+      first_chars += first_vowel_to_end[0]
+      first_vowel_to_end = first_vowel_to_end[1..-1]
+    end
     return "#{first_vowel_to_end}#{first_chars}ay"
   end
 end
 
 def translate(s)
-  piggish_array = s.downcase.split(' ').map do |i|
+  s = s.downcase.split(' ')
+  piggish_array = s.map do |i|
     translate_one_word(i)
   end
   return piggish_array.join(' ')
